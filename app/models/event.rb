802fe1ef061e16f4e3acb439d854bb1bebcc25eb
SSmitt <lalaproger@yandex.ru>
2020-11-03 01:53:45 +0500

    Added comments and subscriptions

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
-
+  # событие принадлежит юзеру
   belongs_to :user
+
+  has_many :comments
+  has_many :subscriptions
+  has_many :subscribers, through: :subscriptions, source: :user
 
   validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
